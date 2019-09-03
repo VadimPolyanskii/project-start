9f1ec720d0edf53c99fb9a95dc8f41674a2b05ee
Vadim <work.arb19@gmail.com>
2019-09-03 16:50:31 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+let arr = ['plum.png' , 'orange.jpg' , 'apple.bmp'];
+
+console.log(arr[0]);
+
+//alert("Hello World!");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+let answer = prompt("А есть ли Вам 18?" , "Да");
+
+console.log(answer);
